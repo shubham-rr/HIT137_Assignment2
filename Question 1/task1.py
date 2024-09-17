e69046abd3de242a42a14da83b74ae13b2b9027a
Shubham Maharjan <shubham.maharjan.2017@gmail.com>
2024-09-17 16:20:51 +0930

    update Question 1/task1.py

--- Question 1/task1.py
+++ Question 1/task1.py
@@ -3,22 +3,32 @@ import os
 import re
 
 file_path = 'files/'
+output_dir = 'Question 1/'
+output_file = os.path.join(output_dir, 'extracted.txt')
 
 def cleaner(column_name, df):
-    if column_name in df.columns:
-        for line in df[column_name]:
-            cleaned_line = re.sub(r'[^a-z\s]', '', line.lower())
-            texts.append(cleaned_line)
+    cleaned_lines = []
+    if column_name in df.columns: # check if column exists
+        lines = df[column_name].dropna()  # Drop null values
+        for line in lines:  # For each line/row in df[column]
+            cleaned_line = re.sub(r'[^a-z\s]', '', line.lower())  # Clean text and convert to lowercase
+            cleaned_lines.append(cleaned_line)
+    return cleaned_lines
 
-# Read CSVs and extract 'text' column
+# Create output directory if it doesn't exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Read CSVs and extract 'TEXT' and 'SHORT-TEXT' columns
 texts = []
 for file in os.listdir(file_path):
-    if file.endswith(".csv"):
+    if file.endswith(".csv"):  # For each CSV file
         df = pd.read_csv(os.path.join(file_path, file))
-        cleaner('TEXT', df)
-        cleaner('SHORT-TEXT', df)
+        texts.extend(cleaner('TEXT', df))  # Extend list with cleaned lines from 'TEXT'
+        texts.extend(cleaner('SHORT-TEXT', df))  # Extend list with cleaned lines from 'SHORT-TEXT'
+
 
 # Write all texts to a single text file
-with open('Question 1/extracted.txt', 'w') as f:
+with open(output_file, 'w') as f:
     for line in texts:
         f.write(line + '\n')
