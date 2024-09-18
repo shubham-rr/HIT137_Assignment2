05055b44d3e1841f062c5291c3143c201d092714
Shubham Maharjan <shubham.maharjan.2017@gmail.com>
2024-09-18 18:51:29 +0930

    Question 2: fix typo

--- Question 2/Question 2_Chapter 2.py
+++ Question 2/Question 2_Chapter 2.py
@@ -55,4 +55,4 @@ def brute_force_decrypt(ciphered_quote):
 
 ciphered_quote = "VZ FRYSVFU VZCNGVRAG NAQ N YVGGYR VAFRPHER V ZNXR ZVFGNXRF V NZ BHG BS PBAGEBY NAQNG GVZRF UNEQ GB UNAQYR OHG VS LBH PNAG UNAQYR ZR NG ZL JBEFG GURA LBH FHER NF URYYQBAG QRFREIR ZR NG ZL ORFG ZNEVYLA ZBAEBR"
 brute_force_decrypt(ciphered_quote)
-# Based on the output, the shift key that gives the oroginal quote is 13.
+# Based on the output, the shift key that gives the original quote is 13.
